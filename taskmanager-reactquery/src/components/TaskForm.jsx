e62f367aac90b53ecacfc68c5222c3508145940f
serhat-bca <sersen@bergen.org>
2025-05-06 15:52:08 -0400

    feat: add task creation functionality with useMutation in TaskForm component

--- taskmanager-reactquery/src/components/TaskForm.jsx
+++ taskmanager-reactquery/src/components/TaskForm.jsx
@@ -1,7 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createTask } from "../services/api";
+
 const TaskForm = () => {
+  const queryClient = useQueryClient();
+
+  const addMutation = useMutation({
+    mutationFn: createTask,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["tasks"]);
+    },
+    onError: () => console.log("Error while adding a task"),
+  });
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Task Submitted");
+    addMutation.mutate({
+      description: event.target.description.value,
+      urgent: false,
+    });
+    event.target.description.value = "";
   };
 
   return (
